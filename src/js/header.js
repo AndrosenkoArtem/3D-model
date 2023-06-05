68d35dce65a275b08855e5935555d1656ace9fbb
AndrosenkoArtem <yixyxtstt@gmail.com>
2023-06-05 13:53:04 +0300

    add

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,5 @@
 (() => {
-  const menuHeader = document.querySelector('.header-lock');
+  const menuHeader = document.querySelector(".header-lock");
   window.onscroll = function () {
     scrollFunction();
   };
@@ -9,9 +9,12 @@
       document.body.scrollTop > 30 ||
       document.documentElement.scrollTop > 30
     ) {
-      menuHeader.classList.add('compact');
+      menuHeader.classList.add("compact");
     } else {
-      menuHeader.classList.remove('compact');
+      menuHeader.classList.remove("compact");
     }
   }
 })();
+/**
+ *
+ */
